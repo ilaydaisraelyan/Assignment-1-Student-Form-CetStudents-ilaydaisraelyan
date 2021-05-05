1b09bd8cc073b8d732a164b9db88101a5cba7ba0
ilaydaisraelyan <55581336+ilaydaisraelyan@users.noreply.github.com>
2021-05-05 23:13:33 +0300

    1

---
+++ CetStudents/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CetStudents
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public DateTime BirthDate { get; set; }
+        
+        public void SetCreationDate(DateTime value)
+        {
+            private DateTime creationDate
+        }
+
+        public void SetLastUpdateDate(DateTime value)
+        {
+        private DateTime LastUpdateDate
+        }
+    }
+}
